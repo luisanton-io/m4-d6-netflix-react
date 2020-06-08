79f3198d2d3419ae2117c8f361799ebbc9de944c
Luis Antonio Canettoli <hello@luisanton.io>
2020-06-08 15:53:11 +0200

    Updated Comments

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { Badge, ListGroup } from "react-bootstrap";
 
-const CommentList = ({ comments }) =>
-  comments.map((comment) => (
-    <ListGroup key={comment._id}>
-      <ListGroup.Item>
-        <Badge pill variant="info" className="mr-3">
-          {comment.rate}
-        </Badge>
-        {comment.comment}
-      </ListGroup.Item>
-    </ListGroup>
-  ));
+class CommentList extends React.Component {
+
+componentWillUnmount() {
+  console.log("byebye")
+}
+
+render() {
+  let comments = this.props.comments
+  return (
+    <>
+    {
+      comments.map((comment) => (
+        <ListGroup key={comment._id}>
+          <ListGroup.Item>
+            <Badge pill variant="info" className="mr-3">
+              {comment.rate}
+            </Badge>
+            {comment.comment}
+          </ListGroup.Item>
+        </ListGroup>
+      ))
+    }
+    </>
+  )
+}
+}
 
 export default CommentList;
